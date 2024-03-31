0dd52e36105491945a7f3767e595b88aad6f8443
amirasalah <amera.salah.89@gmail.com>
2024-03-31 02:34:32 +0200

    adding BE calls

--- src/data-row/index.tsx
+++ src/data-row/index.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react'
+import { Data } from '../types'
 
-export const DataRow:FC = ({ data }) => {
+export const DataRow:FC<{data: Data}> = ( data ) => {
     return (
         <div>{JSON.stringify(data)}</div>
     )
